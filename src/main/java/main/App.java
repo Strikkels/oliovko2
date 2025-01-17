0fc0e1a17a47f71b29c3b61254efe5a02746986d
Strikkels <lauri.a.sorsa@gmail.com>
2025-01-17 13:39:42 +0200

    Versio2

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -18,7 +18,12 @@ public class App
 
         boolean exit = false;
         while(!exit){
-            System.out.println("1) Näytä auton tila, 2) Muokkaa auton merkkiä ja mallia, 3) Kiihdytä autoa, 4) Hidasta autoa, 0) Lopeta ohjelma");  
+            System.out.println("1) Näytä auton tila");
+            System.out.println("2) Muokkaa auton merkkiä ja mallia");
+            System.out.println("3) Kiihdytä autoa");
+            System.out.println("4) Hidasta autoa");
+            System.out.println("0) Lopeta ohjelma");
+
 
             if(sc.hasNext()) {
                 int i = 0;
